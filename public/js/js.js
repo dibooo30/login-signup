33e0637ea6829d542700e9d876c152cda5185afa
ahmedDiab <dibooo30@gmail.com>
2018-02-01 20:22:29 +0200

    login

---
+++ public/js/js.js
@@ -0,0 +1,17 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define, $, JSLINT, brackets */
+$(function () {
+    'use strict';
+
+
+//    $('body').niceScroll({
+//        cursorwidth: "7px",
+//        cursorcolor: '#555',
+//        zindex: 10,
+//        cursorborder: '0',
+//        cursorHeight: 0
+//    });
+//    $('.nicescroll-cursors').css({
+//        height: 0
+//    });
+});
